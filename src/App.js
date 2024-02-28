941cff3e9051945a0a0bc1010e523e9573f3fb95
SHUBHAM-126 <shubhammhatre126@gmail.com>
2024-02-28 22:35:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+
+
+const dictionary = [
+  { word: "React", meaning: "A JavaScript library for building user interfaces." },
+  { word: "Component", meaning: "A reusable building block in React." },
+  { word: "State", meaning: "An object that stores data for a component." }
+]
+
+
+
+function App() {
+
+  const [seachInput, setSearchInput] = useState('')
+  const [searchResult, setSearchResult] = useState('')
+
+  const handleSearch = (e) => {
+    e.preventDefault()
+
+    const result = dictionary.find(item => item.word.toLowerCase() == seachInput.toLowerCase())
+
+    if(result){
+      setSearchResult(result.meaning)
+    }
+    else{
+      setSearchResult('Word not found in the dictionary.')
+    }
+
+  }
+
+  return (
+    <div>
+      <h1>Dictionary App</h1>
+
+      <form onSubmit={handleSearch}>
+        <input type="text" value={seachInput} onChange={e => setSearchInput(e.target.value)} placeholder="Search for a word..." required />
+        <button type="submit">Search</button>
+      </form>
+
+      <h4>Definition:</h4>
+      {searchResult && <p>{searchResult}</p>}
+
+    </div>
+  );
+}
+
+export default App;
